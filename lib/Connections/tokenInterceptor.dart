4c02999a47037bfc29c0347300599cd7a3e6a44f
Muhammad Bilal <49332486+khanswat@users.noreply.github.com>
2023-10-23 15:49:18 +0500

    Update tokenInterceptor.dart

--- lib/Connections/tokenInterceptor.dart
+++ lib/Connections/tokenInterceptor.dart
@@ -6,7 +6,7 @@ class TokenInterceptors extends Interceptor {
   @override
   void onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
-    options.baseUrl = 'https://sihmgtapp.shifa.com.pk';
+    options.baseUrl = 'https://.......';
 
     options.headers['Authorization'] = 'Bearer $token';
 
